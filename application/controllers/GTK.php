007ccea23a2e3d2597795d0a188b07f22c0ad190
Firman Kurnialloh <firmankurnialloh@gmail.com>
2022-06-01 12:30:28 +0700

    Minor Update #2

--- application/controllers/GTK.php
+++ application/controllers/GTK.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class gtk extends CI_Controller
+class Gtk extends CI_Controller
 {
   public function __construct()
   {
--- application/controllers/PD.php
+++ application/controllers/PD.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class pd extends CI_Controller
+class Pd extends CI_Controller
 {
   public function __construct()
   {
