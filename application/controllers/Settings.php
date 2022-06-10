7748698db51bd28ddb4d39755df6649c974349ed
Firman Kurnialloh <firmankurnialloh@gmail.com>
2022-06-11 03:12:29 +0700

    vendors

--- application/controllers/Settings.php
+++ application/controllers/Settings.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-// require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
